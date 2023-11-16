d8a880ac8a1fd90e206822025e31a44f738a82d3
Ravi kumar pandey <94696741+RaviPandey-TheDeveloper@users.noreply.github.com>
2023-11-16 15:26:54 +0530

    Add files via upload

---
+++ storage/framework/views/01ecb8d76217a44eb0f06a772cd8038b4a575dcf.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0.00
+    </div>
+    <strong>&copy; <?php echo e(date('Y')); ?> <a href="/"><?php echo e($setting->nama_perusahaan); ?></a>.</strong> All rights
+    reserved.
+</footer><?php /**PATH C:\Users\hp5cd\Downloads\Compressed\PointofSale-Laravel\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
